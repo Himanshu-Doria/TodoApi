d81a84ceacafb39196d09962d9f06d280523db24
Himanshu-Doria <himanshu.doria@moldedbits.com>
2016-09-20 14:22:53 +0530

    modifies todo json response

--- app/serializers/todo_serializer.rb
+++ app/serializers/todo_serializer.rb
@@ -1,3 +1,9 @@
 class TodoSerializer < ActiveModel::Serializer
-  attributes :id, :content
+  attributes :id, :content, :created_on
+
+
+
+  def created_on
+    object.created_at.strftime("%d %b %Y")
+  end
 end
